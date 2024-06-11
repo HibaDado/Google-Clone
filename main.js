e9e13c9ad6eaa717302c3707bcfb7e8f860150f6
HibaDado <hibadadou1@gmail.com>
2024-06-11 14:58:17 +0300

    edited

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 var bodyslct = document.querySelector("body");
-        var button = document.querySelector(".change-mode");
-        var mainslct = document.querySelector(".main");
-        var footerslct = document.querySelector("footer");
-        var footerInfo = document.querySelector(".info");
-        var footerLocation = document.querySelector(".location");
-        var topBar = document.querySelector(".top-bar");
+var button = document.querySelector(".change-mode");
+var mainslct = document.querySelector(".main");
+var footerslct = document.querySelector("footer");
+var footerInfo = document.querySelector(".info");
+var footerLocation = document.querySelector(".location");
+var topBar = document.querySelector(".top-bar");
 
-        button.addEventListener("click", function() {
-            bodyslct.classList.toggle("dark-mode");
-            mainslct.classList.toggle("dark-mode");
-            footerslct.classList.toggle("dark-mode");
-            footerInfo.classList.toggle("dark-mode");
-            footerLocation.classList.toggle("darker-mode");
-            topBar.classList.toggle("dark-mode");
-        });
+button.addEventListener("click", function () {
+  bodyslct.classList.toggle("dark-mode");
+  mainslct.classList.toggle("dark-mode");
+  footerslct.classList.toggle("dark-mode");
+  footerInfo.classList.toggle("dark-mode");
+  footerLocation.classList.toggle("darker-mode");
+  topBar.classList.toggle("dark-mode");
+});
